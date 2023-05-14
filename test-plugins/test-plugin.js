8b536e10877963e9cdc1e4d78e7f10f0337684c3
AviaAlex <70788432+AviaAlex@users.noreply.github.com>
2023-05-13 23:42:18 -0400

    v1.3.0 (TPJS Fix 2)

--- test-plugins/test-plugin.js
+++ test-plugins/test-plugin.js
@@ -1,12 +1,12 @@
-// Runs the Blookware Help Command
-const pluginName = "WareHelp"
-console.log("Test Plugin")
+// Test Plugin, feel free to make your own plugin using this code!
+const pluginName = "Test Plugin"
+console.log(pluginName)
 console.log("Checking for Blookware...")
 if (typeof bWarePChecker == "function") {
     console.log("Found Blookware. Injecting the plugin via Blookware now...")
     bWarePChecker(pluginName)
     function pluginScript() {
-        blookware_help()
+        // Put your Blookware functions here
     }
     pluginScript()
 }else{
