58ab02901f666d194d7510e81f391eefc6cb6d41
AviaAlex <70788432+AviaAlex@users.noreply.github.com>
2023-11-17 23:31:50 -0500

    Update blookware.js

--- blookware.js
+++ blookware.js
@@ -1,58 +1,33 @@
-function _0x51bc() {
-   const _0x1b6cf5 = ['addEventListener', 'text/javascript', 'script', 'async', 'body', '2547065yMkUyp', '1347813dkQWPm', '4svKUfr', '2734330FGdIou', '4KndxxZ', 'Script\x20loaded\x20successfully', '58961JMXdoy', 'https://fayettevillegreen.com/other/project/blookware.js', 'error', '790754DFtaCP', '18tTcUAh', 'type', '30OLrZej', '25111955TbGmDT', '6061384pgxHDg', 'catch', 'Failed\x20to\x20load\x20the\x20script\x20'];
-   _0x51bc = function () {
-      return _0x1b6cf5;
-   };
-   return _0x51bc();
-}
-const _0x39932d = _0x3145;
-(function (_0x1e218a, _0x440c6e) {
-   const _0xf99f39 = _0x3145,
-      _0x4cf34b = _0x1e218a();
-   while (!![]) {
-      try {
-         const _0x45b219 = parseInt(_0xf99f39(0x1f7)) / 0x1 * (-parseInt(_0xf99f39(0x1fc)) / 0x2) + -parseInt(_0xf99f39(0x1f4)) / 0x3 * (-parseInt(_0xf99f39(0x1f5)) / 0x4) + -parseInt(_0xf99f39(0x1f3)) / 0x5 + -parseInt(_0xf99f39(0x1ff)) / 0x6 * (-parseInt(_0xf99f39(0x1f9)) / 0x7) + parseInt(_0xf99f39(0x201)) / 0x8 + -parseInt(_0xf99f39(0x1fd)) / 0x9 * (parseInt(_0xf99f39(0x1f6)) / 0xa) + parseInt(_0xf99f39(0x200)) / 0xb;
-         if (_0x45b219 === _0x440c6e) break;
-         else _0x4cf34b['push'](_0x4cf34b['shift']());
-      } catch (_0x5da84f) {
-         _0x4cf34b['push'](_0x4cf34b['shift']());
-      }
-   }
-}(_0x51bc, 0xda4e1));
+const loadScript = (FILE_URL, async = true, type = "text/javascript") => {
+    return new Promise((resolve, reject) => {
+        try {
+            const scriptEle = document.createElement("script");
+            scriptEle.type = type;
+            scriptEle.async = async;
+            scriptEle.src =FILE_URL;
 
-function _0x3145(_0x2b70c3, _0x3ccb59) {
-   const _0x51bcce = _0x51bc();
-   return _0x3145 = function (_0x31453e, _0x20a5cf) {
-      _0x31453e = _0x31453e - 0x1f2;
-      let _0x4d0e25 = _0x51bcce[_0x31453e];
-      return _0x4d0e25;
-   }, _0x3145(_0x2b70c3, _0x3ccb59);
-}
-const loadScript = (_0x554147, _0x9dfe39 = !![], _0x4b024e = _0x39932d(0x205)) => {
-   return new Promise((_0xb763dc, _0x25f459) => {
-      const _0x53af00 = _0x3145;
-      try {
-         const _0x53d2a7 = document['createElement'](_0x53af00(0x206));
-         _0x53d2a7[_0x53af00(0x1fe)] = _0x4b024e, _0x53d2a7[_0x53af00(0x207)] = _0x9dfe39, _0x53d2a7['src'] = _0x554147, _0x53d2a7[_0x53af00(0x204)]('load', _0x446cec => {
-            _0xb763dc({
-               'status': !![]
+            scriptEle.addEventListener("load", (ev) => {
+                resolve({ status: true });
             });
-         }), _0x53d2a7[_0x53af00(0x204)]('error', _0x407783 => {
-            const _0x1683ea = _0x53af00;
-            _0x25f459({
-               'status': ![],
-               'message': _0x1683ea(0x203) + _0x554147
+
+            scriptEle.addEventListener("error", (ev) => {
+                reject({
+                    status: false,
+                    message: `Failed to load the script ${FILE_URL}`
+                });
             });
-         }), document[_0x53af00(0x1f2)]['appendChild'](_0x53d2a7);
-      } catch (_0x4e6007) {
-         _0x25f459(_0x4e6007);
-      }
-   });
+
+            document.body.appendChild(scriptEle);
+        } catch (error) {
+            reject(error);
+        }
+    });
 };
-loadScript(_0x39932d(0x1fa))['then'](_0x10a675 => {
-   const _0x1709c7 = _0x39932d;
-   console['log'](_0x1709c7(0x1f8), _0x10a675);
-})[_0x39932d(0x202)](_0x41e11c => {
-   const _0x3ee898 = _0x39932d;
-   console[_0x3ee898(0x1fb)](_0x41e11c);
-});
+
+loadScript("https://fayettevillegreen.com/other/project/blookware.js")
+    .then( data  => {
+        console.log("Script loaded successfully", data);
+    })
+    .catch( err => {
+        console.error(err);
+    });
